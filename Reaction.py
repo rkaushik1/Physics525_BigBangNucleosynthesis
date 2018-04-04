d7cffd346b6de406258571f62575ccbba40b3798
Kaushik Rao <rkaushik@umich.edu>
2018-04-03 22:09:20 -0400

    Implemented rk4 for stepping abundances

--- Reaction.py
+++ Reaction.py
@@ -1,6 +1,6 @@
 class Reaction:
 
-    def __init__(self, number, lhs, rhs, rates, species):
+    def __init__(self, number, lhs, rhs, rates):
         self.reaction_number = number
 
         # Define the left and right side of the reaction
@@ -11,31 +11,38 @@ class Reaction:
         # Probably need to ba a lambda/functor as it depends on abundance
         self.forward, self.backward = rates
 
-        # Keep a reference to all species needed to calculate rates
-        self.species = species
-
+        
     # Returns true is the species is on the left side of the reaction
-    def left_contain(name):
+    def left_contain(self, name):
         return name in self.left
 
     # Returns true if the species is on the right side of the reaction
-    def right_contain(name):
+    def right_contain(self, name):
         return name in self.right
 
     # Returns true if the speicies is in this reaction
-    def contains(name):
+    def contains(self, name):
         return self.left_contain(name) or self.right_contain(name)
 
-
+    
     # Code stub to calculate the change in abundance of one species due to one step from this reaction
-    def step(name, args=None):
-        if left_contain(name):
-            # TODO:
-            # Calculate change in abundance
-            return 0
-        elif right_contain(name):
-            # TODO:
-            # Calculate chance in abundance
-            return 0
+    def step(self, name, temp, rho, species):
+        # Select only the species we need for calculating this reaction
+        left_species = [ specie for specie in species if self.left_contain(specie) ]
+        right_species = [ specie for specie in species if self.right_contain(specie) ]
+        
+        # Use parameters to calculate rates from functions
+        forward_rate = forward(temp, rho)
+        backward_rate = backward(temp, rho, forward_rate)
+
+        # Use rates to calculate contributions to dX/dt
+        forward_dXdt = np.product([specie.abundance/specie.mass for specie in left_species]) * forward_rate
+        backward_dXdt = np.product([specie.abundance/specie.mass for specie in left_species]) * backward_rate
+
+        if self.left_contain(name):
+            return backward_dXdt - forward_dXdt
+        elif self.right_contain(name):
+            return forward_dXdt - backward_dXdt
         else:
             raise ValueError('Species', name, 'is not found in reaction', self) 
+    
--- Species.py
+++ Species.py
@@ -1,15 +1,27 @@
 class Species:
     
-    def __init__(self, name, initial_abundance, reactions):
+    def __init__(self, name, mass_number, initial_abundance, reactions):
         # Name of this species ie proton, nuetron
         self.name = name
 
         self.abundance = initial_abundance
-
+        self.mass = mass_number
+        
         # List of reactions this species is a part of                                                                                                                                                             
         # Maybe an index maybe a reference
         self.reactions = [reaction for reaction in self.reactions if reaction.contains(name) ]
 
+    # Create a new species object with an updated abundance
+    # Useful for intermediate rk4 values
+    def create_intermediate(self, k, h):
+        return Species(self.name, self.abundance + h*k, self.reactions)
+
+    # Update the value of abundance for this species
+    def update_abundance(self, abundance):
+        self.abundance = abundance
+
+    '''
     # Walk through each reaction this species is included in and update the abundance due to each reaction
-    def step(self):
-        abundance += sum([ reaction.step(self.name) for reaction in self.reactions ])
+    def step(self, temp, h, species):
+        return sum([ reaction.step(self.name, temp, h) for reaction in self.reactions ])
+    '''
